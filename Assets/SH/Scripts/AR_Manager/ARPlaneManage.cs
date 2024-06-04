00b8b0487c884eb35e4368bf181d94905eb96f65
Yoon.07.09 <159544864+samhomesss@users.noreply.github.com>
2024-06-04 10:40:59 +0900

    Update SceneLinked

--- Assets/SH/Scripts/AR_Manager/ARPlaneManage.cs
+++ Assets/SH/Scripts/AR_Manager/ARPlaneManage.cs
@@ -8,13 +8,14 @@ public class ARPlaneManage : MonoBehaviour
     public void Start()
     {
         planeManager = GetComponent<ARPlaneManager>();
-        //planeManager.requestedDetectionMode = UnityEngine.XR.ARSubsystems.PlaneDetectionMode.None;
+        //planeManager.requestedDetectionMode = UnityEngine.XR.ARSubsystems.PlaneDetectionMode.None; // 이건 아무것도 인식 안하는거임 
 
         
     }
 
     private void Update()
     {
+        // 매순간 평면 확인을 안보이도록 하는거 
         foreach (var trackable in planeManager.trackables)
         {
             trackable.GetComponent<Renderer>().enabled = false;
