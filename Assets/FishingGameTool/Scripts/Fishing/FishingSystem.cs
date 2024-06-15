1b8eb0236e6b58c847bdbc316dedce12b682addc
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-06-16 07:00:46 +0900

    update

--- Assets/FishingGameTool/Scripts/Fishing/FishingSystem.cs
+++ Assets/FishingGameTool/Scripts/Fishing/FishingSystem.cs
@@ -648,9 +648,9 @@ namespace FishingGameTool.Fishing
             else if (!_castInput && _currentCastForce != 0f) // 던지기 입력이 비활성화되고 던지기 힘이 0이 아니라면 던지기 동작을 시작
             {
                 Vector3 spawnPoint = _fishingRod._line._lineAttachment.position; // 낚싯대의 시작 위치
-                Vector3 castDirection = GetComponentInParent<CameraBobbing>().transform.forward+ Vector3.up;// 던지는 방향
+                Vector3 castDirection = _fishingRod.transform.forward+ Vector3.up;// 던지는 방향
 
-                Debug.Log(GetComponentInParent<CameraBobbing>().transform.forward + "aaa"+ castDirection);
+                Debug.Log(_fishLoadEndPosition.transform.forward + "aaa"+ castDirection);
                 StartCoroutine(SoundManager.instance.SFXPlay("Casting", SoundManager.instance.audioClips[(int)SoundClip.Casting],0.1f, 0.3f));
                 StartCoroutine(CastingDelay(_spawnFloatDelay, castDirection, spawnPoint, _currentCastForce, _fishingFloatPrefab));
                 StartCoroutine(waitDestroyRope());
